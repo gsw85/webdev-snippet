956f22561d7113446ea33107e18b34d5f1f92179
SW. G <gsw.thc@gmail.com>
2023-01-06 13:45:27 +0800

    add comment

--- firebase/cloud-function/index.js
+++ firebase/cloud-function/index.js
@@ -1,11 +1,12 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const helloModule = require("./hello");
 
+// cloud function called helloworld will be created
 exports.helloworld = onRequest(
   {
     timeoutSeconds: 1200,
-    region: ["europe-north1"],
-    cors: ["https://website1.com", "https://www.website2.me"],
+    region: ["europe-north1"], // location of cloud function
+    cors: ["https://website1.com", "https://www.website2.me"], // which website can access this cloud function
   },
   helloModule.handler
 );
